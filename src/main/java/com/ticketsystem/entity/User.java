49a2798b541e6a8f134759f789db034179d72a1f
Vivek Kumar <vivekkumar54458@gmail.com>
2026-05-17 19:53:16 +0530

    user

--- src/main/java/com/ticketsystem/entity/User.java
+++ src/main/java/com/ticketsystem/entity/User.java
@@ -39,6 +39,7 @@ public class User {
     @JoinTable(name = "user_roles",
         joinColumns = @JoinColumn(name = "user_id"),
         inverseJoinColumns = @JoinColumn(name = "role_id"))
+    @Builder.Default
     private Set<Role> roles = new HashSet<>();
 
     @Column(nullable = false, updatable = false)
